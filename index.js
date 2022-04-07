c95cb2f36107fccb0be461fcf84cdb0aea118a90
eyal gerby <eyalg535@gmail.com>
2022-04-07 18:48:58 -0400

    commit tree ded10fdbff94b7cd6b3f058604b8f370237d773d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
+  
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1;
+
+
+const num3 = 22
+const num4 = 6
+let mod = num3 % num4;
+
+let max = 20;
+Math.max(5, 10, 15, 20);
